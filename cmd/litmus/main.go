b4976255c86ce56c0206cf977bd1930c8d9893a6
Thomas Nyambati <12892110+nyambati@users.noreply.github.com>
2026-04-17 12:21:20 +0200

    feat: add --version flag to CLI
    Support both --version and -v short flags showing 'litmus version X.Y.Z' format.

--- cmd/litmus/main.go
+++ cmd/litmus/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -11,13 +10,13 @@ const version = "0.1.0-alpha"
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use:   "litmus",
-		Short: "Litmus - Alertmanager Validator",
-		Long:  "Litmus validates Alertmanager configurations through regression and behavioral testing",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Litmus Alertmanager Validator v%s\n", version)
-		},
+		Use:     "litmus",
+		Short:   "Litmus - Alertmanager Validator",
+		Long:    "Litmus validates Alertmanager configurations through regression and behavioral testing",
+		Version: version,
 	}
+
+	rootCmd.SetVersionTemplate("litmus version {{.Version}}\n")
 
 	rootCmd.AddCommand(newInitCmd())
 	rootCmd.AddCommand(newSnapshotCmd())
